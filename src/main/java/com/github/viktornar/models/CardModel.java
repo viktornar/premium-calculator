30cdbd43aae161dc1317a1be559417c72aa70087
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-07-27 23:44:29 +0300

    Added type for card.
    Basically, card as sub object of contract can have a different type (credit, debit, prepaid and so on).

--- src/main/java/com/github/viktornar/models/CardModel.java
+++ src/main/java/com/github/viktornar/models/CardModel.java
@@ -1,10 +1,12 @@
 package com.github.viktornar.models;
 
+import com.github.viktornar.types.CardType;
 import com.github.viktornar.types.RiskType;
 import lombok.Data;
 
 @Data
 public class CardModel extends BaseModel {
+    CardType cardType;
     Double insuredAmount;
     RiskType riskType;
 }
---
+++ src/main/java/com/github/viktornar/types/CardType.java
@@ -0,0 +1,8 @@
+package com.github.viktornar.types;
+
+public enum CardType {
+    UNKNOWN,
+    DEBIT,
+    CREDIT,
+    PREPAID,
+}
