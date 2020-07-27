c74972257990bc4e3c144a1533185b57a4d21089
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-07-27 22:00:14 +0300

    Added spring boot with some additional dependencies.

---
+++ src/main/java/com/github/viktornar/PremiumCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.viktornar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumCalculatorApplication.class, args);
+	}
+
+}
