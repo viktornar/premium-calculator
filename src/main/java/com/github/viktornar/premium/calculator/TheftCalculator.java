e47389e1d0c51456caf8478340199840b6b2d638
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-07-28 17:04:57 +0300

    Using static imports for constants.

--- src/main/java/com/github/viktornar/premium/calculator/TheftCalculator.java
+++ src/main/java/com/github/viktornar/premium/calculator/TheftCalculator.java
@@ -1,22 +1,24 @@
 package com.github.viktornar.premium.calculator;
 
 import com.github.viktornar.models.CardModel;
-import com.github.viktornar.premium.Constants;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+
+import static com.github.viktornar.premium.Constants.COEFFICIENT_DEFAULT_THEFT;
+import static com.github.viktornar.premium.Constants.COEFFICIENT_EQUAL_OR_GREATER_15_THEFT;
 
 @Slf4j
 public class TheftCalculator implements Calculator {
     @Override
     public double calculatePremium(double sum) {
         if (sum >= 15) {
-            log.debug("Sum is greater or equal than / to 15. {} coefficient will be applied", Constants.COEFFICIENT_EQUAL_OR_GREATER_15_THEFT);
-            return sum * Constants.COEFFICIENT_EQUAL_OR_GREATER_15_THEFT;
+            log.debug("Sum is greater or equal than / to 15. {} coefficient will be applied", COEFFICIENT_EQUAL_OR_GREATER_15_THEFT);
+            return sum * COEFFICIENT_EQUAL_OR_GREATER_15_THEFT;
         }
 
-        log.debug("{} coefficient will be applied", Constants.COEFFICIENT_DEFAULT_THEFT);
-        return sum * Constants.COEFFICIENT_DEFAULT_THEFT;
+        log.debug("{} coefficient will be applied", COEFFICIENT_DEFAULT_THEFT);
+        return sum * COEFFICIENT_DEFAULT_THEFT;
     }
 
     @Override
