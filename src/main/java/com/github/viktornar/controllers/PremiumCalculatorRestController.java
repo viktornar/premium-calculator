651b6b477c184d9dabb7bc4678b5fd737bad0032
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-07-28 15:19:04 +0300

    Added missing @RequestBody anotation.

--- src/main/java/com/github/viktornar/controllers/PremiumCalculatorRestController.java
+++ src/main/java/com/github/viktornar/controllers/PremiumCalculatorRestController.java
@@ -8,6 +8,7 @@ import com.github.viktornar.services.NoCustomerException;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class PremiumCalculatorRestController {
     }
 
     @PostMapping("/premiumForGivenContract")
-    ResultResponse calculateContractPremium(ContractModel contract) throws NotFoundCalculatorException, NoCustomerException, NoCardException {
+    ResultResponse calculateContractPremium(@RequestBody ContractModel contract) throws NotFoundCalculatorException, NoCustomerException, NoCardException {
         double premium = contractPremiumService.getContractPremium(contract);
         return new ResultResponse(contract.getId(), premium);
     }
